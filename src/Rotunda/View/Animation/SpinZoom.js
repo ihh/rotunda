db28a7559c45e581a3d64f56f5bb34ec501e05a3
Ian Holmes <ihh@berkeley.edu>
2016-07-03 12:04:55 -0700

    SpinZoom.js

---
+++ src/Rotunda/View/Animation/SpinZoom.js
@@ -0,0 +1,41 @@
+define(['dojo/_base/declare',
+        'Rotunda/View/Animation'],
+       function(declare,
+                Animation) {
+
+/**
+ * @class
+ */
+return declare (Animation,
+{
+    constructor: function(rotunda, callback, time, newRadians, newScale) {
+        this.rotunda = rotunda
+        this.oldScale = rotunda.scale
+        this.newScale = newScale
+        this.relativeScale = this.newScale / this.oldScale
+        this.zoomingIn = this.relativeScale > 1
+        this.deltaScale = Math.abs (this.newScale - this.oldScale)
+        this.minScale = Math.min (this.oldScale, this.newScale)
+
+        this.degrees = (newRadians - rotunda.rotate) * 180 / Math.PI
+},
+
+    step: function(pos) {
+        var zoomFraction = this.zoomingIn ? pos : 1 - pos
+        var z2 = zoomFraction * zoomFraction
+        var curScaleFactor = (z2 * this.deltaScale + this.minScale) / this.oldScale
+        var nonlinear = this.minScale >= this.rotunda.nonlinearScaleThreshold
+	var xfactor = curScaleFactor, yfactor = curScaleFactor
+	if (nonlinear) {
+	    yfactor = Math.pow (xfactor, this.rotunda.trackRadiusScaleExponent)
+	}
+        this.rotunda.gTransformRotateAndScale (this.degrees*pos, xfactor, yfactor)
+    },
+
+    cleanup: function() {
+	if (this.rotunda.useCanvasForAnimations)
+	    this.rotunda.destroyAnimationCanvas()
+    }
+})
+
+       })
